c67f38b486753b4a92f79197b429dd2297b5e32f
Trần Ngọc Huy <truongduchuy910@gmail.com>
2024-05-19 23:13:54 +0700

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Literata, Merriweather } from "next/font/google";
 import "./globals.css";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header";
 
 const literata = Literata({
   subsets: ["vietnamese"],
